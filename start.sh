5703904b506122731682c8bf468e247c4a01cf21
siyou <128565655+s1you@users.noreply.github.com>
2026-03-25 07:14:41 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@ sudo mkdir /mnt/hdd
 sudo mount /dev/sda3 /mnt/hdd
 
 sudo apt install openjdk-21-jdk-headless -y
+sudo apt install screen
 
 curl -SsL https://playit-cloud.github.io/ppa/key.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/playit.gpg >/dev/null
 echo "deb [signed-by=/etc/apt/trusted.gpg.d/playit.gpg] https://playit-cloud.github.io/ppa/data ./" | sudo tee /etc/apt/sources.list.d/playit-cloud.list
