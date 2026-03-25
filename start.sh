72fc7a75729b974c4a7b7b764de32fce28064f0f
siyou <128565655+s1you@users.noreply.github.com>
2026-03-25 14:32:01 +0900

    Create start.sh for Minecraft server setup
    Script to set up and run Minecraft server with necessary installations.

---
+++ start.sh
@@ -0,0 +1,15 @@
+sudo su
+cd
+sudo mkdir /mnt/hdd
+sudo mount /dev/sda3 /mnt/hdd
+
+sudo apt install openjdk-21-jdk-headless -y
+
+curl -SsL https://playit-cloud.github.io/ppa/key.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/playit.gpg >/dev/null
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/playit.gpg] https://playit-cloud.github.io/ppa/data ./" | sudo tee /etc/apt/sources.list.d/playit-cloud.list
+sudo apt update
+sudo apt install playit
+playit
+
+cd /mnt/hdd/Minecraft
+start.sh
