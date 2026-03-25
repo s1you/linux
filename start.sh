2205c121bd8beb5ae7295b9529e93145bc3d811d
siyou <128565655+s1you@users.noreply.github.com>
2026-03-25 10:30:33 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ sudo mount /dev/sda3 /mnt/hdd
 sudo apt install openjdk-21-jdk-headless -y
 sudo apt install screen
 
-sudo apt install libfuse2
+# sudo apt install libfuse2
 
 curl -SsL https://playit-cloud.github.io/ppa/key.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/playit.gpg >/dev/null
 echo "deb [signed-by=/etc/apt/trusted.gpg.d/playit.gpg] https://playit-cloud.github.io/ppa/data ./" | sudo tee /etc/apt/sources.list.d/playit-cloud.list
