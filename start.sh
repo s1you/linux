fe59518fe220c3f22a4f07274f6ae4cb12cd792f
siyou <128565655+s1you@users.noreply.github.com>
2026-03-26 03:32:01 +0000

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,9 @@
+sudo apt update
+cd
+sudo mkdir /mnt/hdd
+sudo mount /dev/sda3 /mnt/hdd
+
+sudo apt install openjdk-21-jdk-headless -y
+sudo apt install screen
+sudo apt install ufw
+sudo ufw allow 25565/tcp
