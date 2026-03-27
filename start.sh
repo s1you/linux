0d924dd64c53fc82458781af211715f20a9883eb
siyou <128565655+s1you@users.noreply.github.com>
2026-03-27 05:21:30 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,8 +3,11 @@ cd
 sudo mkdir /mnt/hdd
 sudo mount /dev/sda3 /mnt/hdd
 
+sudo apt install ntpsec-ntpdate -y
+sudo ntpdate pool.ntp.org
 sudo apt install openjdk-21-jdk-headless -y
 sudo apt install screen
 sudo apt install ufw -y
+
 sudo ufw allow 25565/tcp
 sudo ufw allow 19132/udp
