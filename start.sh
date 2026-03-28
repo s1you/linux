f58838e02986f5451e8b92e8d231b56ce58efc11
siyou <128565655+s1you@users.noreply.github.com>
2026-03-28 19:12:56 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,6 +5,8 @@ sudo mount /dev/sda3 /mnt/hdd
 
 sudo apt install ntpsec-ntpdate -y
 sudo ntpdate pool.ntp.org
+sudo systemctl restart systemd-timesyncd
+sudo timedatectl set-timezone Asia/Tokyo
 sudo apt install openjdk-21-jdk-headless -y
 sudo apt install screen
 sudo apt install ufw -y
